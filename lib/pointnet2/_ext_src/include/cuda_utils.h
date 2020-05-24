df9ee874df817da0e89e4baab68049c12cceaae5
Ubuntu <chodi150@supermaszyna.etpmrjlcvdtu3egtzvtrlmvm4e.bx.internal.cloudapp.net>
2020-05-24 11:28:10 +0000

    Revert "Updated libraries to support torch 1.4, 1.5"
    This reverts commit 7234b0bcde4740304358fd49b12c5ee1bbc8297f.

--- lib/pointnet2/_ext_src/include/cuda_utils.h
+++ lib/pointnet2/_ext_src/include/cuda_utils.h
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// 
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 #ifndef _CUDA_UTILS_H
 #define _CUDA_UTILS_H
 
--- lib/pointnet2/_ext_src/src/bindings.cpp
+++ lib/pointnet2/_ext_src/src/bindings.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// 
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 #include "ball_query.h"
 #include "group_points.h"
 #include "interpolate.h"
